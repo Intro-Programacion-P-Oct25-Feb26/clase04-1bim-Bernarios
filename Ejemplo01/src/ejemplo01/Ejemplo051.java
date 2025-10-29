73348bc1573b5c8031bdc1048a36c2aaee2f0d10
Daniel Rios <danyberna2002@gmail.com>
2025-10-29 12:15:22 -0500

    repositorio ejemplo051

--- Ejemplo01/src/ejemplo01/Ejemplo051.java
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -11,23 +11,23 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo051 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
         
-        /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
         
         System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
                 nacimiento);
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
-                "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);
+                "\n"+ nacimiento);
+        // System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        // System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);
         // el "\t" significa que esta aplicando un tabulador(4espacios) y el "\n" sirve para dar un salto de linea   
     }
 }
